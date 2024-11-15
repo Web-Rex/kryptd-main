af670713685510775728aae0f543da6824e03e8b
Christopher Victor <victor79646@gmail.com>
2024-11-15 13:34:50 +0100

    new

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      stream: "stream-browserify",
+    },
+  },
+
+  plugins: [
+    react(),
+    NodeGlobalsPolyfillPlugin({
+      buffer: true,
+    })
+  ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 8080, // replace this port with any number you want
+  },
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+    "process.env": process.env,
+  },
+});
