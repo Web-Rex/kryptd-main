6d5e13d01c4f01a286f020946679578436a19cb8
Christopher Victor <victor79646@gmail.com>
2024-11-17 23:13:43 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   resolve: {
@@ -13,7 +14,8 @@ export default defineConfig({
     react(),
     NodeGlobalsPolyfillPlugin({
       buffer: true,
-    })
+    }),
+    nodePolyfills({ include: ['fs', 'stream'] })
   ],
   server: {
     watch: {
